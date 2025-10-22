e81c1a8cb2f67a77c718cb39377c1761f58fa3be
Aditya Sinha <adityasinha1190@gmail.com>
2025-10-22 22:36:06 +0530

    New Changes

---
+++ 01-Practice Programs/Set-01/Input_Value_002.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Input_Value_002 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in); //Scanner Object Creation
+        System.out.print("Enter Your Name : ");
+        String val =scn.nextLine(); //Storing String Input
+        System.out.println("Hi,"+val+" Welcome Back to the World..."); //Final Output
+
+    }
+}
